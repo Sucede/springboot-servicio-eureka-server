f33ea444e586b711939b15dbe1aafef8a13accac
Daniel Fernandez <dfernandez@certiorem.com>
2020-02-14 10:39:08 +0100

    primer commit microservicio Eureka Server

---
+++ springboot-eureka-server-service/src/test/java/com/sucede/sb/app/eureka/SpringbootEurekaServerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sucede.sb.app.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEurekaServerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
